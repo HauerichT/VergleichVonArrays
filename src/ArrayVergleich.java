569f31eb72b3730968eed2d04426728375b9a2f0
timohaverich <timo.haverich@fh-bielefeld.de>
2022-11-07 19:53:28 +0100

    Mit Sortierung

--- src/ArrayVergleich.java
+++ src/ArrayVergleich.java
@@ -1,14 +1,20 @@
-public class VergleichVonArrays {
+public class ArrayVergleich {
 
+    // Methode zum Sortieren eines Arrays (Bubblesort Algorithmus)
     public void sortArrays(int[] array) {
 
+        // Speicherung der Zwischenwerte
         int lower;
         int higher;
+
+        // Speicherung des Status zur Beendigung
         boolean run = true;
 
+        // iteriert über das übergebene Array
         for (int i = 0; i < array.length && run; i++) {
             run = false;
 
+            // sortiert das Array nach der Größe der Integer
             for (int j = 0; j < array.length-1; j++) {
                 if (array[j] > array[j + 1]) {
                     higher = array[j];
@@ -18,17 +24,22 @@ public class VergleichVonArrays {
                     run = true;
                 }
             }
+
         }
 
     }
 
+    // Methode zur Überprüfung zweier Arrays auf Übereinstimmung
     public boolean isEqual (int[] a, int[] b) {
 
+        // ruft die Methode sortArrays auf und übergibt jeweils ein Array
         sortArrays(a);
         sortArrays(b);
 
+        // wenn die Arrays unterschiedlich lang sind, wird false returnt
         if (a.length != b.length) return false;
 
+        // iteriert über beide sortierten Arrays und prüft, ob die Arrays identisch sind
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < b.length; j++) {
                 if (a[i] != b[i]) {
---
+++ src/ArrayVergleichTest.java
@@ -0,0 +1,17 @@
+public class ArrayVergleichTest {
+
+    public static void main(String[] args) {
+
+        // Testarrays
+        int[] a = {3,5,1,3};
+        int[] b = {1,5,3,3};
+
+        // Instanziierung eines neuen ArrayVergleich-Objektes
+        ArrayVergleich arrayVergleich = new ArrayVergleich();
+
+        // führt die Methode isEqual aus, übergibt zwei Arrays und printet das Ergebnis
+        System.out.println(arrayVergleich.isEqual(a, b));
+
+    }
+
+}
