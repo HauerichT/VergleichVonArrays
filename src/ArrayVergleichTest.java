5dad4bb92720a5b1cb4e1c1e7ae271a080e9f89d
timohaverich <timo.haverich@fh-bielefeld.de>
2022-11-08 16:44:44 +0100

    Mit Sortierung

--- src/ArrayVergleichTest.java
+++ src/ArrayVergleichTest.java
@@ -3,8 +3,11 @@ public class ArrayVergleichTest {
     public static void main(String[] args) {
 
         // Testarrays
-        int[] a = {3,5,1,3};
-        int[] b = {1,5,3,3};
+        int[] a = {1,2,3,4};
+        int[] b = {3,1,6,2};
+
+        // int[] a = {3,5,1,3};
+        // int[] b = {1,5,3,3};
 
         // Instanziierung eines neuen ArrayVergleich-Objektes
         ArrayVergleich arrayVergleich = new ArrayVergleich();
